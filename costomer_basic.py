603b47de6bda6009f51d16a1ddfc0f5cd0e009f4
sixdayz6 <70253711+sixdayz6@users.noreply.github.com>
2025-09-08 12:15:51 +0900

    Input code

--- costomer_basic.py
+++ costomer_basic.py
@@ -1,5 +1,5 @@
 import re
-custlist=[]
+custlist=[{'name':'kim','gender':'M',"email":'test@email.com',"birthyear":'2000'}]
 page=-1
 
 
@@ -14,10 +14,58 @@ while True:
     D - 고객 정보 삭제
     Q - 프로그램 종료
     ''')  
-
+    # 대문자로 변경
+    choice = choice.upper()
+    print(choice)
     if choice=="I":        
         print("고객 정보 입력")
         customer={'name':'','gender':'',"email":'',"birthyear":''}
+        while(True):
+            while(True):
+                name = input("이름을 입력해주세요.")
+                if (name.isalpha()):
+                    customer[name] = name
+                    break
+                else:
+                    print("이름은 문자로만 입력해주세요.")
+            
+            while(True):
+                gender = input("""성별을 입력해주세요.
+                성별은 M,F,m,f 만 입력가능합니다.""").upper()
+                if gender == "M" or gender == "F":
+                    customer[gender] = gender
+                    break
+                else:
+                    print("성별은 M,F,m,f 만 입력 가능합니다.")
+            
+            while(True):
+                email = input("이메일을 입력해주세요.")
+                check = 0
+                for i in custlist:
+                    if i[email] == email:
+                        print("이미 있는 이메일 입니다.")
+                        check = 1
+                        break
+
+                if check == 0:
+                    if re.search("@", email) == "@":
+                        customer[email] = email
+                        break
+                    else:
+                        print("이메일 형식 example@email.com")
+            
+            while(True):
+                customer["birthyear"] = input("출생년도 4자리를 입력해주세요.")
+                if str(customer["birthyear"].isdigit()) and len(customer["birthyear"] == 4):
+                    customer["birthyear"] = int(customer["birthyear"])
+                    break
+                else:
+                    print("출생년도는 숫자 4자리를 입력해주세요.")
+
+            print(customer)
+            custlist.append    
+        
+
     elif choice=="C":
         print("현재 고객 정보 조회")
     elif choice == 'P':
@@ -31,3 +79,5 @@ while True:
     elif choice=="Q":
         print("프로그램 종료")
         break
+    else:
+        print("메뉴 선택을 잘못하셨습니다. 다시 선택해 주세요.")
